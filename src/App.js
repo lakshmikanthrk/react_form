0fa3d79291a29fa5ea0f9f0a7cb25d6e3ad07115
lakshmikanth <lakshmikanthrk054.com>
2022-07-17 00:32:52 +0530

    test

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
+import { useForm } from 'react-hook-form';
 import logo from './logo.svg';
 import './App.css';
+import { userState } from 'react';
+
 
 function App() {
+  const { register, handleSubmit,  errors} = useForm();
+  const {userInfo,setUserInfo} = userState();
+  const onSubmit = (data) => {
+    setUserInfo(data);
+      console.log(data);
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <pre>{JSON.stringify(userInfo,undefined,2)}</pre>
+     <form onSubmit={handlesubmit(onSubmit)}>
+      <h1 className='text-center'>Registration Form</h1>
+      <div className='ui divider'>
+        <div className='ui form'>
+        <div class="form-group">
+    <label>Username</label>
+    <input type="text" class="form-control"  name='username' placeholder='username'/>
+  </div>
+          <div class="form-group">
+    <label>Email</label>
+    <input type="email" class="form-control" name='email' placeholder='Email' />
+  </div>
+  <div class="form-group">
+    <label>Password</label>
+    <input type="password" class="form-control" name='password' placeholder='Password' />
+  </div>
+  <div className='text-center'>
+  <button type="submit" class="btn btn-primary">Submit</button>
+  </div>
+        </div>
+      </div>
+     </form>
     </div>
   );
 }
